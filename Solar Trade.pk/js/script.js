25abb7a4dd7be9d43efab184185a79fa15188b27
waqar <waqarattariz10@gmail.com>
2024-08-25 10:03:14 +0500

    nkjhb

---
+++ Solar Trade.pk/js/script.js
@@ -0,0 +1,8 @@
+// var content = document.getElementsByTagName('body')[0];
+// var darkmode = document.getElementById('dark-change');
+// darkmode.addEventListener('click', function(){
+//     darkmode.classList.toggle('active');
+//     content.classList.toggle('night');
+
+
+// })
